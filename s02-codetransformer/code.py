b2ec7c8943ce8b43167399da6d997535c3be8b76
jiangkedong <kadenjiang.se@gmail.com>
2026-07-24 14:30:22 +0800

    feat: 完成dropout-mask attention class

--- s02-codetransformer/code.py
+++ s02-codetransformer/code.py
@@ -101,33 +101,154 @@
 # print(context_vec_2)
 # ---------------------------------------------------------------
 
-# 抽象为class
-import torch.nn as nn
-import torch
-class SelfAttention_v1(nn.Module):
-    def __init__(self, d_in, d_out):
-        super().__init__()
-        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+# # 抽象为class
+# import torch.nn as nn
+# import torch
+# class SelfAttention_v1(nn.Module):
+#     def __init__(self, d_in, d_out):
+#         super().__init__()
+#         self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+#         self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+#         self.W_value = nn.Parameter(torch.rand(d_in, d_out))
     
-    def forward(self, x):
-        keys = x @ self.W_key
-        queries = x @ self.W_query
-        values = x @ self.W_value
+#     def forward(self, x):
+#         keys = x @ self.W_key
+#         queries = x @ self.W_query
+#         values = x @ self.W_value
 
-        attn_scores = queries @ keys.T
-        attn_weights = torch.softmax(
-            attn_scores / keys.shape[-1]**0.5, dim=-1
-        )
-        context_vec = attn_weights @ values
+#         attn_scores = queries @ keys.T
+#         attn_weights = torch.softmax(
+#             attn_scores / keys.shape[-1]**0.5, dim=-1
+#         )
+#         context_vec = attn_weights @ values
 
-        return context_vec
+#         return context_vec
 
-d_in = 3
-d_out = 2
-torch.manual_seed(123)
-sa_v1 = SelfAttention_v1(d_in, d_out)
+# d_in = 3
+# d_out = 2
+# torch.manual_seed(123)
+# sa_v1 = SelfAttention_v1(d_in, d_out)
+# inputs = torch.tensor(
+#     [
+#         [0.43, 0.15, 0.89], # Your (x^1)
+#         [0.55, 0.87, 0.66], # journey (x^2)
+#         [0.57, 0.85, 0.64], # starts (x^3)
+#         [0.22, 0.58, 0.33], # with (x^4)
+#         [0.77, 0.25, 0.10], # one (x^5)
+#         [0.05, 0.80, 0.55]] # step (x^6
+# )
+# print(sa_v1(inputs))
+# class SelfAttention_v2(nn.Module):
+#     def __init__(self, d_in, d_out, qkv_bias=False):
+#         super().__init__()
+#         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    
+#     def forward(self, x):
+#         keys = self.W_key(x)
+#         queries = self.W_query(x)
+#         values = self.W_value(x)
+#         attn_scores = queries @ keys.T
+#         attn_weights = torch.softmax(
+#             attn_scores / keys.shape[-1]**0.5, dim=-1
+#         )
+
+#         context_vec = attn_weights @ values
+
+#         return context_vec
+
+# torch.manual_seed(789)
+# sa_v2 = SelfAttention_v2(d_in, d_out)
+# print(sa_v2(inputs))
+
+# with torch.no_grad():
+#     sa_v1.W_query.copy_(sa_v2.W_query.weight.T)
+#     sa_v1.W_key.copy_(sa_v2.W_key.weight.T)
+#     sa_v1.W_value.copy_(sa_v2.W_value.weight.T)
+# print(torch.allclose(sa_v1(inputs), sa_v2(inputs)))
+# print(sa_v1(inputs))
+# print(sa_v2(inputs))
+# ---------------------------------------------------------------
+
+# # 掩码注意力
+# import torch
+# import torch.nn as nn
+
+# class SelfAttention_v2(nn.Module):
+#     def __init__(self, d_in, d_out, qkv_bias=False):
+#         super().__init__()
+#         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    
+#     def forward(self, x):
+#         keys = self.W_key(x)
+#         queries = self.W_query(x)
+#         values = self.W_value(x)
+#         attn_scores = queries @ keys.T
+#         attn_weights = torch.softmax(
+#             attn_scores / keys.shape[-1]**0.5, dim=-1
+#         )
+
+#         context_vec = attn_weights @ values
+
+#         return context_vec
+
+# d_in = 3
+# d_out = 2
+# torch.manual_seed(789)
+# sa_v2 = SelfAttention_v2(d_in, d_out)
+# inputs = torch.tensor(
+#     [
+#         [0.43, 0.15, 0.89], # Your (x^1)
+#         [0.55, 0.87, 0.66], # journey (x^2)
+#         [0.57, 0.85, 0.64], # starts (x^3)
+#         [0.22, 0.58, 0.33], # with (x^4)
+#         [0.77, 0.25, 0.10], # one (x^5)
+#         [0.05, 0.80, 0.55]] # step (x^6
+# )
+# queries = sa_v2.W_query(inputs)
+# keys = sa_v2.W_key(inputs)
+# attn_scores = queries @ keys.T
+# attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+# print(attn_weights)
+
+# context_length = attn_scores.shape[0]
+# mask_simple = torch.tril(torch.ones(context_length, context_length))
+# print(mask_simple)
+
+# masked_simple = attn_weights * mask_simple
+# print(masked_simple)
+
+# row_sums = masked_simple.sum(dim=-1, keepdim=True)
+# print(row_sums)
+
+# masked_simple_norm = masked_simple / row_sums
+# print(masked_simple_norm)
+
+# # better mask attention, 将掩码的部分设置为-inf, 这样在算softmax时候,概率就为零
+# mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+# masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+# print(masked)
+
+# attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=-1)
+# print(attn_weights)
+
+# # droup mask attention
+# torch.manual_seed(123)
+# dropout = torch.nn.Dropout(0.5)
+# example = torch.ones(6, 6)
+# print(dropout(example))
+
+# torch.manual_seed(123)
+# print(dropout(attn_weights))
+# ---------------------------------------------------------------
+
+# 抽象 drop-mask attention
+import torch
+import torch.nn as nn
+
 inputs = torch.tensor(
     [
         [0.43, 0.15, 0.89], # Your (x^1)
@@ -137,35 +258,50 @@ inputs = torch.tensor(
         [0.77, 0.25, 0.10], # one (x^5)
         [0.05, 0.80, 0.55]] # step (x^6
 )
-print(sa_v1(inputs))
-class SelfAttention_v2(nn.Module):
-    def __init__(self, d_in, d_out, qkv_bias=False):
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch.shape)
+
+# 输入: [batch, num_tokens, d_in] 输出: [batch, num_tokens, d_out] 为所有的token embedding生成对应的context embedding
+class CausalAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
+        self.d_out = d_out
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
-    
-    def forward(self, x):
-        keys = self.W_key(x)
-        queries = self.W_query(x)
-        values = self.W_value(x)
-        attn_scores = queries @ keys.T
-        attn_weights = torch.softmax(
-            attn_scores / keys.shape[-1]**0.5, dim=-1
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
         )
 
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        queries = self.W_query(x)
+        keys = self.W_key(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)
+        # 对注意力得分使用掩码
+        attn_scores.masked_fill(
+            self.mask.bool()[:num_tokens, :num_tokens], 
+            -torch.inf
+        )
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5,
+            dim=-1
+        )
+        # drop 防止过拟合
+        attn_weights = self.dropout(attn_weights)
         context_vec = attn_weights @ values
 
         return context_vec
 
-torch.manual_seed(789)
-sa_v2 = SelfAttention_v2(d_in, d_out)
-print(sa_v2(inputs))
-
-with torch.no_grad():
-    sa_v1.W_query.copy_(sa_v2.W_query.weight.T)
-    sa_v1.W_key.copy_(sa_v2.W_key.weight.T)
-    sa_v1.W_value.copy_(sa_v2.W_value.weight.T)
-print(torch.allclose(sa_v1(inputs), sa_v2(inputs)))
-print(sa_v1(inputs))
-print(sa_v2(inputs))
+torch.manual_seed(123)
+context_length = batch.shape[1]
+d_in = 3
+d_out = 2
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+context_vec = ca(batch)
+print(context_vec.shape)
