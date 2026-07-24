92ecf33f5aa658494752cd914a1c0f7974e5032d
jiangkedong <kadenjiang.se@gmail.com>
2026-07-24 16:46:29 +0800

    feat: 实现多头注意力;优化计算

--- s02-codetransformer/code.py
+++ s02-codetransformer/code.py
@@ -245,63 +245,174 @@
 # print(dropout(attn_weights))
 # ---------------------------------------------------------------
 
-# 抽象 drop-mask attention
-import torch
-import torch.nn as nn
+# # 抽象 drop-mask attention
+# import torch
+# import torch.nn as nn
 
-inputs = torch.tensor(
-    [
-        [0.43, 0.15, 0.89], # Your (x^1)
-        [0.55, 0.87, 0.66], # journey (x^2)
-        [0.57, 0.85, 0.64], # starts (x^3)
-        [0.22, 0.58, 0.33], # with (x^4)
-        [0.77, 0.25, 0.10], # one (x^5)
-        [0.05, 0.80, 0.55]] # step (x^6
-)
-batch = torch.stack((inputs, inputs), dim=0)
-print(batch.shape)
+# inputs = torch.tensor(
+#     [
+#         [0.43, 0.15, 0.89], # Your (x^1)
+#         [0.55, 0.87, 0.66], # journey (x^2)
+#         [0.57, 0.85, 0.64], # starts (x^3)
+#         [0.22, 0.58, 0.33], # with (x^4)
+#         [0.77, 0.25, 0.10], # one (x^5)
+#         [0.05, 0.80, 0.55]] # step (x^6
+# )
+# batch = torch.stack((inputs, inputs), dim=0)
+# print(batch.shape)
 
-# 输入: [batch, num_tokens, d_in] 输出: [batch, num_tokens, d_out] 为所有的token embedding生成对应的context embedding
-class CausalAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
-        super().__init__()
-        self.d_out = d_out
-        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+# # 输入: [batch, num_tokens, d_in] 输出: [batch, num_tokens, d_out] 为所有的token embedding生成对应的context embedding
+# class CausalAttention(nn.Module):
+#     def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+#         super().__init__()
+#         self.d_out = d_out
+#         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
-        self.dropout = nn.Dropout(dropout)
-        self.register_buffer(
-            'mask',
-            torch.triu(torch.ones(context_length, context_length), diagonal=1)
-        )
+#         self.dropout = nn.Dropout(dropout)
+#         self.register_buffer(
+#             'mask',
+#             torch.triu(torch.ones(context_length, context_length), diagonal=1)
+#         )
 
-    def forward(self, x):
-        b, num_tokens, d_in = x.shape
-        queries = self.W_query(x)
-        keys = self.W_key(x)
-        values = self.W_value(x)
+#     def forward(self, x):
+#         b, num_tokens, d_in = x.shape
+#         queries = self.W_query(x)
+#         keys = self.W_key(x)
+#         values = self.W_value(x)
 
-        attn_scores = queries @ keys.transpose(1, 2)
-        # 对注意力得分使用掩码
-        attn_scores.masked_fill(
-            self.mask.bool()[:num_tokens, :num_tokens], 
-            -torch.inf
-        )
-        attn_weights = torch.softmax(
-            attn_scores / keys.shape[-1]**0.5,
-            dim=-1
-        )
-        # drop 防止过拟合
-        attn_weights = self.dropout(attn_weights)
-        context_vec = attn_weights @ values
+#         attn_scores = queries @ keys.transpose(1, 2)
+#         # 对注意力得分使用掩码
+#         attn_scores.masked_fill_(
+#             self.mask.bool()[:num_tokens, :num_tokens], 
+#             -torch.inf
+#         )
+#         attn_weights = torch.softmax(
+#             attn_scores / keys.shape[-1]**0.5,
+#             dim=-1
+#         )
+#         # drop 防止过拟合
+#         attn_weights = self.dropout(attn_weights)
+#         context_vec = attn_weights @ values
 
-        return context_vec
+#         return context_vec
 
-torch.manual_seed(123)
-context_length = batch.shape[1]
-d_in = 3
-d_out = 2
-ca = CausalAttention(d_in, d_out, context_length, 0.0)
-context_vec = ca(batch)
-print(context_vec.shape)
+# torch.manual_seed(123)
+# context_length = batch.shape[1]
+# d_in = 3
+# d_out = 2
+# ca = CausalAttention(d_in, d_out, context_length, 0.0)
+# context_vec = ca(batch)
+# print(context_vec.shape)
+
+# # multi-head self-attention
+# class MultiHeadAttentionWrapper(nn.Module):
+#     def __init__(self, d_in, d_out, context_length,
+#                 dropout, num_heads, qkv_bias=False):
+#         super().__init__()
+#         self.heads = nn.ModuleList(
+#             [CausalAttention(
+#                 d_in, d_out, context_length, dropout, qkv_bias
+#             )
+#             for _ in range(num_heads)]
+#         )
+    
+#     def forward(self, x):
+#         return torch.cat([head(x) for head in self.heads], dim=-1)
+
+# torch.manual_seed(123)
+# context_length = batch.shape[1]
+# d_in, d_out = 3, 2
+
+# mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2)
+# context_vecs = mha(batch)
+# print(context_vecs)
+# print("context_vecs shape:", context_vecs.shape)
+
+# # multi-head self-attention 计算优化
+# class MultiHeadAttention(nn.Module):
+#     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+#         super().__init__()
+#         assert (d_out % num_heads == 0), \
+#             "d_out must be divisible by num_heads"
+
+#         self.d_out = d_out
+#         self.num_heads = num_heads
+#         self.head_dim = d_out // num_heads # 每个头输出context embedding的维度
+#         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+#         self.out_proj = nn.Linear(d_out, d_out)
+#         self.dropout = nn.Dropout(dropout)
+
+#         self.register_buffer(
+#             'mask',
+#             torch.triu(torch.ones(context_length, context_length), diagonal=1)
+#         )
+
+#     def forward(self, x):
+#         b, num_tokens, d_in = x.shape
+#         keys = self.W_key(x)
+#         queries = self.W_query(x)
+#         values = self.W_value(x)
+
+#         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+#         values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+#         queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+#         keys = keys.transpose(1, 2)
+#         values = values.transpose(1, 2)
+#         queries = queries.transpose(1, 2)
+
+#         attn_scores = queries @ keys.transpose(2, 3)
+#         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+#         attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+#         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+#         attn_weights = self.dropout(attn_weights)
+
+#         context_vec = (attn_weights @ values).transpose(1, 2)
+#         context_vec = context_vec.contiguous().view(
+#             b, num_tokens, d_out
+#         )
+
+#         context_vec = self.out_proj(context_vec)
+#         return context_vec
+    
+# a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573],[0.8993, 0.0390, 0.9268, 0.7388],[0.7179, 0.7058, 0.9156, 0.4340]],[[0.0772, 0.3565, 0.1479, 0.5331],[0.4066, 0.2318, 0.4545, 0.9737],[0.4606, 0.5159, 0.4220, 0.5786]]]])
+# print(a @ a.transpose(2, 3))
+
+# first_head = a[0, 0, :, :]
+# print("First head:\n", first_head @ first_head.T)
+
+# second_head = a[0, 1, :, :]
+# print("Second head:\n", second_head @ second_head.T)
+
+# torch.manual_seed(123)
+# batch_size, context_length, d_in = batch.shape
+# d_out = 2
+# mha = MultiHeadAttention(d_in, d_out, context_length, 0.0,
+#                         num_heads=2)
+# context_vecs = mha(batch)
+# print(context_vecs)
+# print("context_vecs.shape:", context_vecs.shape)
+
+# # Exercise 3.3 Initializing GPT-2 size attention modules Using the MultiHeadAttention class, initialize a multi-head attention module that has the same number of attention heads as the smallest GPT-2 model (12 attention heads). Also ensure that you use the respective input and output embedding sizes similar to GPT-2 (768 dimensions). Note that the smallest GPT-2 model supports a context length of 1,024 tokens
+# import torch
+# torch.manual_seed(123)
+# inputs = torch.rand(1024, 768)
+# print("inputs.shape: ", inputs.shape)
+# print(inputs[:2])
+
+# batch = torch.stack((inputs, inputs), dim=0)
+
+# batch_size, context_length, d_in = batch.shape
+# d_out = 768
+# mha = MultiHeadAttention(d_in, d_out, context_length, 0.0,
+#                         num_heads=12)
+# context_vecs = mha(batch)
+# print(context_vecs)
+# print("context_vec.shape:", context_vecs.shape)
+# ---------------------------------------------------------------
